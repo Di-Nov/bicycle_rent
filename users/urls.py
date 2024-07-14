f26cdf619ad77572a0f8ba0f526152a3e9b69ad7
dima <pieero47@gmail.com>
2024-07-14 11:21:58 +0300

    create router for User

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
+from views import UserViewSet
+
+router = SimpleRouter()
+router.register('users', UserViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
