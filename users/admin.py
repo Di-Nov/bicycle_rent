19067c221d41edbb5d62a7fddfacd09c9b5681fe
dima <pieero47@gmail.com>
2024-07-14 11:41:07 +0300

    create admin for user

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+
+# admin.site.register(CustomUser)
+
+@admin.register(CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        'username',
+        'first_name',
+        'last_name',
+        'email',
+        'is_active',
+        'date_joined',
+        'date_berth',
+    ]
+    list_display_links = ['username']
+    readonly_fields = ['image', 'date_joined']
+    search_fields = ['username__icontains', 'first_name__icontains', 'last_name__icontains', 'email__icontains']
+    list_per_page = 15
+    list_editable = ['is_active']
+    ordering = ['date_joined', 'username']
+    list_filter = ['is_active', 'date_joined', 'date_berth',]
+    save_on_top = True
+
+
